6891bbd0b05d5123c87c9027fe1e31e62b7f0e52
Slighten <zz687@yahoo.com.tw>
2015-01-22 05:06:21 +0800

    1/22 05:01 Final_V3

--- app/src/main/java/netdb/courses/softwarestudio/labs/pushnotification/AlarmMessage.java
+++ app/src/main/java/netdb/courses/softwarestudio/labs/pushnotification/AlarmMessage.java
@@ -129,7 +129,7 @@ public class AlarmMessage extends ActionBarActivity {
             editor.putInt("counter", ++counter);
             editor.apply();
         }else if (counter == 2) {
-            Toast.makeText(AlarmMessage.this, "Snooze again " + ((min > 0) ? ((min == 1) ? min + " minite": min + " minutes") : sec + " seconds") + ".\nNext time I'll call someone else.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(AlarmMessage.this, "Snooze again " + ((min > 0) ? ((min == 1) ? min + " minite": min + " minutes") : sec + " seconds") + ".\nNext time I'll call someone else.", Toast.LENGTH_LONG).show();
             msg.setText("DELETE");
             editor.putInt("counter", ++counter);
             editor.apply();
@@ -141,8 +141,8 @@ public class AlarmMessage extends ActionBarActivity {
             it.setAction(Intent.ACTION_CALL);
             it.setData(uri);
             startActivity(it);
-            msg.setText("DELETE ALARM");
-            Toast.makeText(AlarmMessage.this, "To delete snooze, click \"DELETE\" button on main screen.", Toast.LENGTH_SHORT).show();
+            msg.setText("DELETE");
+            Toast.makeText(AlarmMessage.this, "To delete snooze, click \"DELETE\" button on main screen.", Toast.LENGTH_LONG).show();
         }
         // StaticWakeLock.lockOff(context);
         AlarmMessage.this.finish();
